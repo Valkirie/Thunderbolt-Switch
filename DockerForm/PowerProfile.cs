de459b5f4ce49a637359932d0cb1a91d6a66cf7c
Lesueur Benjamin <valkiriebc@hotmail.fr>
2021-02-06 18:29:44 +0100

    removed unused functions

--- DockerForm/PowerProfile.cs
+++ DockerForm/PowerProfile.cs
@@ -16,33 +16,6 @@ namespace DockerForm
         PluggedIn = 0x02  // 0000 0000 0000 0010
     }
 
-    public static class FlagsHelper
-    {
-        public static bool IsSet<T>(T flags, T flag) where T : struct
-        {
-            int flagsValue = (int)(object)flags;
-            int flagValue = (int)(object)flag;
-
-            return (flagsValue & flagValue) != 0;
-        }
-
-        public static void Set<T>(ref T flags, T flag) where T : struct
-        {
-            int flagsValue = (int)(object)flags;
-            int flagValue = (int)(object)flag;
-
-            flags = (T)(object)(flagsValue | flagValue);
-        }
-
-        public static void Unset<T>(ref T flags, T flag) where T : struct
-        {
-            int flagsValue = (int)(object)flags;
-            int flagValue = (int)(object)flag;
-
-            flags = (T)(object)(flagsValue & (~flagValue));
-        }
-    }
-
     public static class StringExtension
     {
         public static string GetLast(this string source, int tail_length)
